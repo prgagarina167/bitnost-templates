6a83a8515e6ecd707b0940b192ef39ccb09bee04
fimdomeio <alexandre@fimdomeio.com>
2015-04-07 12:35:54 +0100

    removing js log message.

--- build/js/script.js
+++ build/js/script.js
@@ -3,7 +3,6 @@ $(function () {
   $('[data-toggle="popover"]').popover()
 
   $( ".item" ).mouseleave(function() {
-  	console.log('mouseleft');
   	if($(this)[0] !== $('.popover').parent()[0]){
   		$('.popover').popover('hide');
   	}
